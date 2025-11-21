c25d08a4daab2748f8dc969de04afb0595ab20c2
maonamao <2582480639@qq.com>
2025-11-21 11:37:29 +0800

    backend基础

---
+++ Backend/majorWork/src/main/java/org/example/majorwork/MajorWorkApplication.java
@@ -0,0 +1,13 @@
+package org.example.majorwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajorWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MajorWorkApplication.class, args);
+	}
+
+}
---
+++ Backend/majorWork/src/test/java/org/example/majorwork/MajorWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.majorwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajorWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
